bd0b23250748da211a9b8e06df87836be7aa1bfd
Martin Sustrik <sustrik@250bpm.com>
2020-07-10 06:21:23 +0200

    add CSS

--- convert.sh
+++ convert.sh
@@ -6,5 +6,6 @@ for d in *; do
   if [ -d "$d" ]; then
     echo "$d"
     md-to-html --input $d/blog.md --output $d/t.html
+    sed -i '5i    <link rel="stylesheet" type="text/css" href="../main.css">' $d/t.html
   fi
 done
