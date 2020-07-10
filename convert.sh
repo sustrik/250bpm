1ef2ca663ef5e2d0f2fd8d174437591b8bda5715
Martin Sustrik <sustrik@250bpm.com>
2020-07-10 06:55:02 +0200

    regenerate html with showdown

--- convert.sh
+++ convert.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-md-to-html --input all.md --output all.html
+showdown makehtml all.md -o all.html
 
 for d in *; do
   if [ -d "$d" ]; then
     echo "$d"
-    md-to-html --input $d/blog.md --output $d/t.html
-    sed -i '5i    <link rel="stylesheet" type="text/css" href="../main.css">' $d/t.html
+    showdown makehtml $d/blog.md -o $d/t.html
   fi
 done
