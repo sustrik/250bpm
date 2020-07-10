6307b5669c558fd9378e153d7f0a1d2f3e603901
Martin Sustrik <sustrik@250bpm.com>
2020-07-10 07:31:43 +0200

    regenerate html

--- convert.sh
+++ convert.sh
@@ -5,6 +5,6 @@ showdown makehtml all.md -o all.html
 for d in *; do
   if [ -d "$d" ]; then
     echo "$d"
-    showdown makehtml $d/blog.md -o $d/t.html
+    showdown makehtml -i $d/blog.md -o $d/t.html
   fi
 done
