ee867a75e6eb11c7457d852e1ad730af8edd7258
Martin Sustrik <sustrik@250bpm.com>
2020-07-21 20:08:26 +0200

    ToC geneted within convert.sh

--- convert.sh
+++ convert.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+./maketoc.py
 showdown makehtml -i toc.md -o toc.html
 cat header.html toc.html footer.html > index.html
+rm toc.md toc.html
+exit
 
 for d in *; do
   if [ -d "$d" ]; then
