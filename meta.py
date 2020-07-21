03a3938ebb74e403112758485804d1dfd94eb379
Martin Sustrik <sustrik@250bpm.com>
2020-07-21 08:04:42 +0200

    Add meta.json files

--- meta.py
+++ meta.py
@@ -3,11 +3,9 @@ for i in range(1, 161):
   with open('%d/blog.md' % i, 'r') as f:
     c = f.read()
   lines = c.strip().split('\n')
-  print("---- " + str(i))
-  print(lines[0])
-  print(lines[-1])  
-
-
-  #with open("all.md", "w") as f:
-  #  f.write(out)
+  title = lines[0][2:]
+  date = lines[-1][2:-2]
+  meta = """{"title": "%s", "date": "%s"}""" % (title, date)
+  with open("%d/meta.json" % i, "w") as f:
+    f.write(meta)
 
