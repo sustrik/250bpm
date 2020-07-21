a4c3840785ce3aacb63c9aad4d5278852508b78f
Martin Sustrik <sustrik@250bpm.com>
2020-07-21 20:06:20 +0200

    script to generate ToC

---
+++ maketoc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+
+# collect blog titles
+titles = {}
+for d in os.listdir():
+  if not os.path.isdir(d):
+    continue
+  if not d.startswith('blog:'):
+    continue
+  num = int(d[5:])
+  with open(d + '/blog.md', 'r') as f:
+    c = f.read()
+  title = c.strip().split('\n')[0][2:]
+  titles[num] = title
+
+out = ''
+for blog in sorted(titles.items()):
+  out += "* [%s](blog:%d/index.html)\n" % (blog[1], blog[0])
+
+with open("toc.md", "w") as f:
+    f.write(out)
+
