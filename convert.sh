59053adfa89f9f8a6827fa2b516c9601c7b624d5
Martin Sustrik <sustrik@250bpm.com>
2020-07-21 22:50:20 +0200

    a lot of stuff

--- convert.sh
+++ convert.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-
-
 ./maketoc.py
 
 showdown makehtml -i toc.md -o toc.raw.html
@@ -11,7 +9,6 @@ showdown makehtml -i index.md -o index.raw.html
 cat header.html index.raw.html footer.html > index.html
 
 rm toc.md toc.raw.html index.md index.raw.html
-exit
 
 for d in *; do
   if [ -d "$d" ]; then
--- maketoc.py
+++ maketoc.py
@@ -18,16 +18,49 @@ for d in os.listdir():
 blogs = sorted(titles.items())
 blogs.reverse()
 
-out = ''
-for blog in blogs:
-  out += "* [%s](/250bpm/blog:%d)\n" % (blog[1], blog[0])
-with open("toc.md", "w") as f:
+def fmt(blog):
+  return "* [%s](/250bpm/blog:%d)\n" % (blog[1], blog[0])
+
+out = """
+# 250bpm
+
+My name is Martin Sústrik.
+
+Once I have written software projects such as [ØMQ](https://zeromq.org/), [nanomsg](https://nanomsg.org) or [libdill](https://libdill.org).
+
+I believe I was the first one to use the term [Structured Concurrency](https://en.wikipedia.org/wiki/Structured_concurrency).
+
+These days I blog about random stuff.
+
+All the opinions stated here are my own. Any resemblance to opinions of other people, living or dead, is purely coincidental.
+
+"""
+
+out += '### Recent\n\n'
+for i in range(0, 5):
+  out += fmt(blogs[i])
+
+out += '### Sociology, Politology, History, Coordination Problems\n\n'
+for i in [160, 159, 151, 136, 135, 132, 128, 127, 125, 113, 100, 96, 94, 92, 66]:
+  out += fmt((i, titles[i]))  
+
+out += '### Short Stories\n\n'
+for i in [150, 134, 131, 130, 129, 109]:
+  out += fmt((i, titles[i]))  
+
+out += '### Moments Musicaux\n\n'
+for i in [148, 84, 79]:
+  out += fmt((i, titles[i]))
+
+out += '### All articles\n\n'
+out += '* [All articles](toc.html)\n\n'
+
+with open("index.md", "w") as f:
     f.write(out)
 
-out = '### Recent\n\n'
-for i in range(0, 5):
-  blog = blogs[i]
-  out += "* [%s](/250bpm/blog:%d)\n" % (blog[1], blog[0])
-with open("index.md", "w") as f:
+out = ''
+for blog in blogs:
+  out += fmt(blog)
+with open("toc.md", "w") as f:
     f.write(out)
 
